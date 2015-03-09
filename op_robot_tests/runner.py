2aafde643719738a0d8a7474b6ba03e7a33dac24
Taras Kozlovskyi <ktarasz@quintagroup.com>
2015-03-09 17:19:44 +0200

    Improved runner

--- op_robot_tests/runner.py
+++ op_robot_tests/runner.py
@@ -5,5 +5,12 @@ import sys
 
 def runner():
     args = sys.argv[1:]
+    if '-s' not in args:
+        args += ['-s', 'OpenProcurementProcess']
+    if '-d' not in args:
+        directory = os.path.join(os.getcwd(), 'test_output')
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+        args += ['-d', directory]
     args.append(os.path.join(os.path.dirname(__file__), 'tests_files'))
     return run_cli(args)
