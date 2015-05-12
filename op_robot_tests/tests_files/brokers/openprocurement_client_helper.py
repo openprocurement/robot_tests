b3978b1c1f99225f0c4a331469a9e29707ab4908
Andrew Yanovych <yankee7@quintagroup.com>
2015-05-12 13:39:35 +0300

    added api version selection

--- op_robot_tests/tests_files/brokers/openprocurement_client_helper.py
+++ op_robot_tests/tests_files/brokers/openprocurement_client_helper.py
@@ -1,5 +1,5 @@
 from openprocurement_client.client import Client
 
 
-def prepare_api_wrapper(key='', host_url="https://api-sandbox.openprocurement.org"):
-    return Client(key, host_url)
+def prepare_api_wrapper(key='', host_url="https://api-sandbox.openprocurement.org", api_version='0.7' ):
+    return Client(key, host_url, api_version )
