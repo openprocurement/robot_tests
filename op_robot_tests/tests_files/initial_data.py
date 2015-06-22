bdcd2dfa455bdae5c2ec7b4a80fe95a20df766c9
Andrew Yanovych <yankee7@quintagroup.com>
2015-06-22 16:10:37 +0300

    passed intervals to initial data as arguments

--- op_robot_tests/tests_files/initial_data.py
+++ op_robot_tests/tests_files/initial_data.py
@@ -82,6 +82,7 @@ def test_tender_data(period_interval=2):
         }
     ],
     "enquiryPeriod": {
+        "startDate": (now).isoformat(),
         "endDate": (now + timedelta(minutes=1)).isoformat()
     },
     "tenderPeriod": {
@@ -175,129 +176,13 @@ def prom_test_tender_data():
     }
 }
 
-def test_tender_data_multiple_lots1():  
+def test_tender_data_multiple_lots(period_interval=2):  
     now = datetime.now()
     return {
       "title": fake.catch_phrase(),
       "mode": "test",
       "submissionMethodDetails": "quick",
-      "description": "Тестовий тендер",
-      "description_en": "Test tender",
-      "description_ru": "Тестовый тендер",
-      "procuringEntity": {
-	  "name": fake.company(),
-	  "name_ru": fake_ru.company(),
-	  "name_en": fake_en.company(),
-	  "identifier": {
-	      "scheme": u"UA-EDR",
-	      "id": u"0000{}".format(fake.pyint()),
-	      "uri": fake.image_url(width=None, height=None)
-	  },
-	  "address": {
-	      "countryName": u"Україна",
-	      "postalCode": fake.postalcode(),
-	      "region": u"м. Київ",
-	      "locality": u"м. Київ",
-	      "streetAddress": fake.street_address()
-	  },
-	  "contactPoint": {
-	      "name": fake.name(),
-	      "telephone": fake.phone_number()
-	  }
-      },
-      "value": {
-	  "amount": 50000.99,
-	  "currency": u"UAH"
-      },
-      "minimalStep": {
-	  "amount": 100.1,
-	  "currency": u"UAH"
-      },
-    "items": [
-        {
-            "description": fake.catch_phrase(),
-            "deliveryDate": {
-                "endDate": (now + timedelta(days=5)).isoformat()
-            },
-            "deliveryLocation": {
-                "latitude": 49.8500,
-                "longitude": 24.0167
-            },
-            "deliveryAddress": {
-                "countryName": u"Україна",
-                "postalCode": fake.postalcode(),
-                "region": u"м. Київ",
-                "locality": u"м. Київ",
-                "streetAddress": fake.street_address()
-            },
-            "classification": {
-                "scheme": u"CPV",
-                "id": u"44617100-9",
-                "description": u"Cartons"
-            },
-            "additionalClassifications": [
-                {
-                    "scheme": u"ДКПП",
-                    "id": u"17.21.1",
-                    "description": u"папір і картон гофровані, паперова й картонна тара"
-                }
-            ],
-            "unit": {
-                "name": u"кілограм",
-                "code": u"KGM"
-            },
-            "quantity": fake.pyint()
-        },
-	{
-            "description": fake.catch_phrase(),
-            "deliveryDate": {
-                "endDate": (now + timedelta(days=5)).isoformat()
-            },
-            "deliveryLocation": {
-                "latitude": 49.8500,
-                "longitude": 24.0167
-            },
-            "deliveryAddress": {
-                "countryName": u"Україна",
-                "postalCode": fake.postalcode(),
-                "region": u"м. Київ",
-                "locality": u"м. Київ",
-                "streetAddress": fake.street_address()
-            },
-            "classification": {
-                "scheme": u"CPV",
-                "id": u"44617100-9",
-                "description": u"Cartons"
-            },
-            "additionalClassifications": [
-                {
-                    "scheme": u"ДКПП",
-                    "id": u"17.21.1",
-                    "description": u"картон гофровані, паперова й картонна тара"
-                }
-            ],
-            "unit": {
-                "name": u"кілограм",
-                "code": u"KGM"
-            },
-            "quantity": fake.pyint()
-        }
-    ],
-    "enquiryPeriod": {
-        "endDate": (now + timedelta(minutes=2)).isoformat()
-    },
-    "tenderPeriod": {
-        "endDate": (now + timedelta(minutes=5)).isoformat()
-    }
-}
-
-def test_tender_data_multiple_lots():  
-    now = datetime.now()
-    return {
-      "title": fake.catch_phrase(),
-      "mode": "test",
-      "submissionMethodDetails": "quick",
-      "description": "Тестовий тендер",
+      "description": u"Тестовий тендер",
       "description_en": "Test tender",
       "description_ru": "Тестовый тендер",
       "procuringEntity": {
@@ -468,10 +353,12 @@ def test_tender_data_multiple_lots():
         }
     ],
     "enquiryPeriod": {
-        "endDate": (now + timedelta(minutes=120)).isoformat()
+        "startDate": (now).isoformat(),
+        "endDate": (now + timedelta(minutes=1)).isoformat()
     },
     "tenderPeriod": {
-        "endDate": (now + timedelta(minutes=120)).isoformat()
+        "startDate": (now + timedelta(minutes=2)).isoformat(),
+        "endDate": (now + timedelta(minutes=(2+period_interval))).isoformat()
     }
 }    
 
