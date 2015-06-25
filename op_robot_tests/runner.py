058d76eadd60084dc3edbad3c9e8bf2bb70ccaf1
Taras Kozlovskyi <ktarasz@quintagroup.com>
2015-06-25 15:55:05 +0300

    Fixed problem with chromedriver

--- op_robot_tests/runner.py
+++ op_robot_tests/runner.py
@@ -1,12 +1,13 @@
 from robot import run_cli
 import os
 import sys
+from chromedriver import CHROMEDRV_PATH
+
+sys.path.append(CHROMEDRV_PATH)
 
 
 def runner():
     args = sys.argv[1:]
-    #if '-s' not in args:
-    #    args += ['-s', 'OpenProcurementProcess']
     if '-d' not in args:
         directory = os.path.join(os.getcwd(), 'test_output')
         if not os.path.exists(directory):
