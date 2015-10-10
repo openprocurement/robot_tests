3cc3c0b90714bbf7a2638b16faec69aba82a4050
selurvedu <selurvedu@users.noreply.github.com>
2015-10-10 19:30:02 +0000

    Improve PEP8 compliance in op_client_helper.py

--- op_robot_tests/tests_files/brokers/openprocurement_client_helper.py
+++ op_robot_tests/tests_files/brokers/openprocurement_client_helper.py
@@ -1,10 +1,11 @@
 from openprocurement_client.client import Client
 import sys
 
-def prepare_api_wrapper(key='', host_url="https://api-sandbox.openprocurement.org", api_version='0.8' ):
-    return Client(key, host_url, api_version )
+
+def prepare_api_wrapper(key='', host_url="https://api-sandbox.openprocurement.org", api_version='0.8'):
+    return Client(key, host_url, api_version)
+
 
 def get_internal_id(get_tenders_function, date):
-	result = get_tenders_function({"offset": date, "opt_fields": 'tenderID', })
-	#import pdb; pdb.Pdb(stdout=sys.__stdout__).set_trace()
-	return result
+    result = get_tenders_function({"offset": date, "opt_fields": 'tenderID'})
+    return result
