ea24974502e0e293905b493d0993ab2fc1812192
selurvedu <selurvedu@users.noreply.github.com>
2015-11-13 22:04:05 +0000

    Remove unused import; split a long line of code

--- op_robot_tests/tests_files/brokers/openprocurement_client_helper.py
+++ op_robot_tests/tests_files/brokers/openprocurement_client_helper.py
@@ -1,8 +1,9 @@
 from openprocurement_client.client import Client
-import sys
 
 
-def prepare_api_wrapper(key='', host_url="https://api-sandbox.openprocurement.org", api_version='0.8'):
+def prepare_api_wrapper(key='',
+                        host_url='https://api-sandbox.openprocurement.org',
+                        api_version='0.8'):
     return Client(key, host_url, api_version)
 
 
