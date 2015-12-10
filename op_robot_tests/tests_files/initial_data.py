efe60708f6c8f28dd6970c56228fc8caec85630b
selurvedu <selurvedu@users.noreply.github.com>
2015-12-10 19:50:20 +0000

    Shorten test_tender_data_multiple_lots
    This reduces unnecessary duplication of code.

--- op_robot_tests/tests_files/initial_data.py
+++ op_robot_tests/tests_files/initial_data.py
@@ -214,44 +214,8 @@ def prom_test_tender_data():
 
 def test_tender_data_multiple_lots(period_interval):
     now = get_now()
-    return {
-        "title": fake.catch_phrase(),
-        "mode": "test",
-        "submissionMethodDetails": "quick",
-        "description": u"Тестовий тендер",
-        "description_ru": u"Тестовый тендер",
-        "description_en": "Test tender",
-        "procuringEntity": {
-            "name": fake.company(),
-            "name_ru": fake_ru.company(),
-            "name_en": fake_en.company(),
-            "identifier": {
-                "scheme": u"UA-EDR",
-                "id": u"0000{}".format(fake.pyint()),
-                "uri": fake.image_url(width=None, height=None)
-            },
-            "address": {
-                "countryName": u"Україна",
-                "countryName_ru": u"Украина",
-                "countryName_en": "Ukraine",
-                "postalCode": fake.postalcode(),
-                "region": u"м. Київ",
-                "locality": u"м. Київ",
-                "streetAddress": fake.street_address()
-            },
-            "contactPoint": {
-                "name": fake.name(),
-                "telephone": fake.phone_number()
-            }
-        },
-        "value": {
-            "amount": 50000.99,
-            "currency": u"UAH"
-        },
-        "minimalStep": {
-            "amount": 100.1,
-            "currency": u"UAH"
-        },
+    t_data = test_tender_data(period_interval)
+    t_data.update({
         "items": [
             {
                 "description": fake.catch_phrase(),
@@ -413,16 +377,9 @@ def test_tender_data_multiple_lots(period_interval):
                 },
                 "quantity": fake.pyint()
             }
-        ],
-        "enquiryPeriod": {
-            "startDate": (now).isoformat(),
-            "endDate": (now + timedelta(minutes=1)).isoformat()
-        },
-        "tenderPeriod": {
-            "startDate": (now + timedelta(minutes=2)).isoformat(),
-            "endDate": (now + timedelta(minutes=(2 + period_interval))).isoformat()
-        }
-    }
+        ]
+    })
+    return t_data
 
 
 def test_question_data():
