2d9fc0ba9c38b3d026e804033cd6c54fb7c6f11e
Leits <leits@quintagroup.org>
2016-01-11 17:15:27 +0200

    Multilot tender test

--- op_robot_tests/tests_files/service_keywords.py
+++ op_robot_tests/tests_files/service_keywords.py
@@ -16,13 +16,14 @@ from robot.output.loggerhelper import Message
 from .initial_data import (
     auction_bid, create_fake_doc, test_award_data, test_bid_data,
     test_bid_data_meat_tender, test_complaint_data, test_complaint_reply_data,
-    test_invalid_features_data, test_item_data, test_meat_tender_data,
-    test_question_answer_data, test_question_data, test_tender_data,
+    test_invalid_features_data, test_item_data, test_lot_complaint_data,
+    test_lot_data, test_lot_document_data, test_lot_question_data,
+    test_lots_bid_data, test_meat_tender_data, test_question_answer_data,
+    test_question_data, test_tender_data, test_tender_data_multiple_items,
     test_tender_data_multiple_lots
 )
 from .local_time import get_now, TZ
 import os
-import calendar
 from barbecue import chef
 
 
@@ -109,7 +110,7 @@ def prepare_test_tender_data(period_intervals, mode):
     if mode == 'single':
         return munchify({'data': test_tender_data(period_intervals)})
     elif mode == 'multi':
-        return munchify({'data': test_tender_data_multiple_lots(period_intervals)})
+        return munchify({'data': test_tender_data_multiple_items(period_intervals)})
     raise ValueError('Invalid mode for test_tender_data')
 
 
