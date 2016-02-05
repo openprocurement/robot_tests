a4a329f109d68bb6dfa8bca86c6d765e0e860d9a
Roman Zaporozhets <r.zaporozhets@prom.ua>
2016-02-05 17:21:31 +0200

    change data/brokers.yaml data/users.yaml service_keywords.py

--- op_robot_tests/tests_files/service_keywords.py
+++ op_robot_tests/tests_files/service_keywords.py
@@ -203,3 +203,41 @@ def convert_datetime_to_dot_format(isodate):
 
 def local_path_to_file(file_name):
     return os.path.join(os.path.dirname(__file__), 'documents', file_name)
+
+
+## Prom.ua
+def get_all_prom_dates(period_interval=31):
+    now = datetime.now()
+    return {
+        'EndPeriod': (now + timedelta(minutes=8)).strftime("%d.%m.%Y %H:%M"),
+        'StartDate': (now + timedelta(minutes=8)).strftime("%d.%m.%Y %H:%M"),
+        'EndDate': (now + timedelta(minutes=(8 + period_interval))).strftime("%d.%m.%Y %H:%M"),
+    }
+
+def convert_date_to_prom_tender(isodate):
+    first_iso = datetime.strptime(isodate, "%d.%m.%y").isoformat()
+    return first_iso
+
+
+def convert_date_to_prom_tender_startdate(isodate):
+    first_date = isodate.split(' - ')[0]
+    first_iso = datetime.strptime(first_date, "%d.%m.%y %H:%M").isoformat()
+    return first_iso
+
+def convert_date_to_prom_tender_enddate(isodate):
+    second_date = isodate.split(' - ')[1]
+    second_iso = datetime.strptime(second_date, "%d.%m.%y %H:%M").isoformat()
+    return second_iso
+
+
+def procuringEntity_name_prom(INITIAL_TENDER_DATA):
+    INITIAL_TENDER_DATA.data.procuringEntity['name'] = u"Test_company_from_Prozorro"
+    return INITIAL_TENDER_DATA
+
+
+def convert_prom_string_to_common_string(string):
+    return {
+        u"Украина": u"Україна",
+        u"Киевская область": u"м. Київ",
+        u"килограммы": u"кілограм",
+    }.get(string, string)
