13eff401dc7d3278301b6ea8ef75ef7d05a2f043
mykhaly <yurkamykhalchuk@gmail.com>
2016-03-14 12:59:31 +0200

    Update `Відповісти на вимогу` to work with complaintID
    Deleted claim_id from arguments in test_claim_answer_data, now it is inserted
    into dict in op_client.robot keyword 'Відповісти на вимогу'
    Update singleItemTenderComplaints to work with this changes

--- op_robot_tests/tests_files/initial_data.py
+++ op_robot_tests/tests_files/initial_data.py
@@ -357,14 +357,13 @@ def test_claim_answer_satisfying_data(claim_id):
     })
 
 
-def test_claim_answer_data(claim_id):
+def test_claim_answer_data():
     return munchify({
         "data": {
             "status": "answered",
             "resolutionType": "resolved",
             "tendererAction": fake.sentence(nb_words=10, variable_nb_words=True),
-            "resolution": fake.sentence(nb_words=15, variable_nb_words=True),
-            "id": claim_id
+            "resolution": fake.sentence(nb_words=15, variable_nb_words=True)
         }
     })
 
