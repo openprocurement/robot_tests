4c94ba2ab044c2d21b4bc1688b10ce41a09f79c2
Leits <leits@quintagroup.org>
2016-05-30 14:09:51 +0300

    Stable API client

--- op_robot_tests/tests_files/brokers/openprocurement_client_helper.py
+++ op_robot_tests/tests_files/brokers/openprocurement_client_helper.py
@@ -1,10 +1,23 @@
 from openprocurement_client.client import Client
 from openprocurement_client.utils import get_tender_id_by_uaid
 from openprocurement_client.exceptions import IdNotFound
+from restkit.errors import RequestFailed
+from retrying import retry
+
+
+def retry_if_request_failed(exception):
+    return isinstance(exception, RequestFailed)
+
+
+class StableClient(Client):
+    @retry(stop_max_attempt_number=100, wait_random_min=500, wait_random_max=4000, retry_on_exception=retry_if_request_failed)
+    def request(self, *args, **kwargs):
+        return super(StableClient, self).request(*args, **kwargs)
 
 
 def prepare_api_wrapper(key, host_url, api_version):
-    return Client(key, host_url, api_version)
+    return StableClient(key, host_url, api_version)
+
 
 def get_complaint_internal_id(tender, complaintID):
     try:
@@ -21,4 +34,3 @@ def get_complaint_internal_id(tender, complaintID):
     except AttributeError:
         pass
     raise IdNotFound
-
