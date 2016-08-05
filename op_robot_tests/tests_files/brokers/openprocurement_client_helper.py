13ff46a419ad4359286f14f8c6f4b3eae5152f05
mykhaly <yurkamykhalchuk@gmail.com>
2016-08-05 18:02:03 +0300

    Fix get_document_by_id
    To find document in awards.complaints.documents

--- op_robot_tests/tests_files/brokers/openprocurement_client_helper.py
+++ op_robot_tests/tests_files/brokers/openprocurement_client_helper.py
@@ -50,6 +50,10 @@ def get_document_by_id(data, doc_id):
         for document in award.get('documents', []):
             if doc_id in document.get('title', ''):
                 return document
+        for complaint in award.get('complaints', []):
+            for document in complaint.get('documents', []):
+                if doc_id in document.get('title, ''):
+                    return document
     raise Exception('Document with id {} not found'.format(doc_id))
 
 
