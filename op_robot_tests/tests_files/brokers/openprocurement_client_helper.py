760ef5340dce4ade1c6082df769a65687f850beb
mykhaly <yurkamykhalchuk@gmail.com>
2016-08-08 19:54:20 +0300

    Add section for search in cancellations
    Into `get_document_by_id` in order to find documents in cancelation.robot

--- op_robot_tests/tests_files/brokers/openprocurement_client_helper.py
+++ op_robot_tests/tests_files/brokers/openprocurement_client_helper.py
@@ -54,6 +54,10 @@ def get_document_by_id(data, doc_id):
             for document in complaint.get('documents', []):
                 if doc_id in document.get('title', ''):
                     return document
+    for cancellation in data.get('cancellations', []):
+        for document in cancellation.get('documents', []):
+            if doc_id in document.get('title', ''):
+                return document
     raise Exception('Document with id {} not found'.format(doc_id))
 
 
