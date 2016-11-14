d14de09f9031c450ac33807274aec26e7af74008
annawzz <annawzz@quintagroup.org>
2016-11-14 18:31:54 +0200

    Add support for faking VDR URLs
    Generate new valid URLs for each run

--- op_robot_tests/tests_files/service_keywords.py
+++ op_robot_tests/tests_files/service_keywords.py
@@ -24,6 +24,7 @@ from .initial_data import (
     create_fake_image,
     create_fake_minimal_step,
     create_fake_sentence,
+    create_fake_vdr_url,
     fake,
     test_bid_data,
     test_bid_value,
