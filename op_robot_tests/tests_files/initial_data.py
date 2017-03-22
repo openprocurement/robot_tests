d2458c05dbb6611cf04a14610654e9a86f4a4ae7
ivanka-jakimchuk <ivanka-jakimchuk@quintagroup.org>
2017-03-22 17:26:01 +0200

    Change bid_value_amount

--- op_robot_tests/tests_files/initial_data.py
+++ op_robot_tests/tests_files/initial_data.py
@@ -311,11 +311,11 @@ def test_bid_data():
     return bid
 
 
-def test_bid_value(max_value_amount):
+def test_bid_value(max_value_amount, minimalStep):
     return munchify({
         "value": {
             "currency": "UAH",
-            "amount": round(random.uniform(max_value_amount, max_value_amount * 1.05), 2),
+            "amount": round(random.uniform(1, 1.05)*(max_value_amount + minimalStep), 2),
             "valueAddedTaxIncluded": True
         }
     })
