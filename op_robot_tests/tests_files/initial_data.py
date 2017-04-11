ab098aa9e66dd79682874682dd8623c3df1ccd50
kosaniak <yulia_kosaniak@ukr.net>
2017-04-11 10:14:41 +0300

    Change randomer for tenderAttempts

--- op_robot_tests/tests_files/initial_data.py
+++ op_robot_tests/tests_files/initial_data.py
@@ -59,7 +59,7 @@ def create_fake_dgfDecisionID():
 
 
 def create_fake_tenderAttempts():
-    return fake.random_int(min=1, max=3)
+    return fake.random_int(min=1, max=4)
 
 
 def create_fake_amount():
@@ -500,7 +500,7 @@ def test_tender_data_dgf_financial(params):
     data['dgfID'] = fake.dgfID()
     data['dgfDecisionID'] = fake.dgfDecisionID()
     data['dgfDecisionDate'] = (get_now() + timedelta(days=-2)).strftime('%Y-%m-%d')
-    data['tenderAttempts'] = fake.random_int(min=1, max=4)
+    data['tenderAttempts'] = fake.random_int(min=1, max=3)
     del data["procuringEntity"]
 
     for i in range(params['number_of_items']):
