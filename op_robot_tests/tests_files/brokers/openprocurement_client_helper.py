b0518bcc3f5b07515590be8c171beb144e3c51c8
ivanka12 <ivanka1211@ukr.net>
2017-08-09 16:35:44 +0300

    update retry_if_request_failed

--- op_robot_tests/tests_files/brokers/openprocurement_client_helper.py
+++ op_robot_tests/tests_files/brokers/openprocurement_client_helper.py
@@ -11,12 +11,10 @@ import os
 import urllib
 
 def retry_if_request_failed(exception):
-    if isinstance(exception, RequestFailed):
-        status_code = getattr(exception, 'status_int', None)
-        if 500 <= status_code < 600 or status_code in (409, 429):
-            return True
-        else:
-            return False
+    status_code = getattr(exception, 'status_code', None)
+    print(status_code)
+    if 500 <= status_code < 600 or status_code in (409, 429, 412):
+        return True
     else:
         return isinstance(exception, BadStatusLine)
 
