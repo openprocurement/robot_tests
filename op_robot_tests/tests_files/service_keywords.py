38c1a83297ba3c00d49e6aa69503fc670be93343
kosaniak <yulia_kosaniak@ukr.net>
2017-09-07 13:00:38 +0300

    Change generation bid data for dgfInsider

--- op_robot_tests/tests_files/service_keywords.py
+++ op_robot_tests/tests_files/service_keywords.py
@@ -493,11 +493,13 @@ def generate_test_bid_data(tender_data):
         bid.data.update(test_bid_value(tender_data['value']['amount'], tender_data['minimalStep']['amount']))
     if 'dgfOtherAssets' in tender_data.get('procurementMethodType', ''):
         bid.data.qualified = True
-    if 'dgfFinancialAssets' or 'dgfInsider' in tender_data.get('procurementMethodType', ''):
+    if 'dgfFinancialAssets' in tender_data.get('procurementMethodType', ''):
         bid.data.eligible = True
         bid.data.qualified = True
         bid.data.tenderers[0]["additionalIdentifiers"] = [fake.additionalIdentifier()]
     if 'dgfInsider' in tender_data.get('procurementMethodType', ''):
+        bid.data.eligible = True
+        bid.data.qualified = True
         del bid.data['value']
     return bid
 
