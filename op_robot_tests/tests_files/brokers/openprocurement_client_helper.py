5def7e08170e47eaf443b2c91cd515bef96f80d2
orest.shkhumov <orest.shkhumov@gmail.com>
2020-02-22 09:59:09 +0200

    delete line

--- op_robot_tests/tests_files/brokers/openprocurement_client_helper.py
+++ op_robot_tests/tests_files/brokers/openprocurement_client_helper.py
@@ -36,12 +36,12 @@ class StableDsClient(DocumentServiceClient):
         return super(StableDsClient, self).request(*args, **kwargs)
 
 
-
 class StableCriteriaClient(CriteriaServiceClient):
     @retry(stop_max_attempt_number=100, wait_random_min=500,
            wait_random_max=4000, retry_on_exception=retry_if_request_failed)
     def request(self, *args, **kwargs):
         return super(StableCriteriaClient, self).request(*args, **kwargs)
+
 
 class StableAgreementClient(AgreementClient):
     @retry(stop_max_attempt_number=100, wait_random_min=500,
